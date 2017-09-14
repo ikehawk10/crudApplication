9c977ca6242541e551a03d4f6e67f743c08376a0
Isaac <hawkins.isaac@gmail.com>
2017-09-14 11:38:35 -0500

    handle any errors in the console

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const data = { 
+		title, 
+		author, 
+		body
+	};
+const { id } = req.params;
 
 mongoose.connect('mongodb://ikehawk10:jesus100@ds133814.mlab.com:33814/articles');
 
@@ -13,42 +19,33 @@ const Article = mongoose.model('articles', {
 });
 
 app.set('view engine', 'ejs');
+
 //extract data from the form inputs
 app.use(bodyParser.urlencoded({extended: true}));
 
+//homescreen to render index.html
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-//get all the articles from the database and render to the page
+//get ALL the articles from the database and render to the page
 app.get('/view-articles', (req, res) => {
 	Article.find({}, (err, articles) => {
+		if (err) return console.log(err);
 		res.render('view-articles', {articles});
 	});
 });
 
+//render newly updated article
 app.post('/articles/:id', (req, res) => {
-	const { id } = req.params
-	const { title, author, body } = req.body
-
-	Article.findOneAndUpdate({_id: id}, { 
-		title, 
-		author, 
-		body
-	}, (err, result) => {
+	Article.findOneAndUpdate({_id: id}, data, (err, result) => {
+		if (err) return console.log(err);
 		res.redirect('/view-articles');
-	});
-})
+	})
+});
 
+//add article to the mongodb collection
 app.post('/articles', (req, res) => {
-	//add user input into the mongodb collection
-	const { title, author, body } = req.body
-	const data = { 
-		title, 
-		author, 
-		body
-	}
-
 	Article.create(data, (err, result) => {
 		if (err) return console.log(err);
 		//redirect back to articles page
@@ -56,18 +53,18 @@ app.post('/articles', (req, res) => {
 	})
 })
 
-app.get('/articles/:id', (req, res) => {
-	const { id } = req.params
-	
+//get specific article by id to edit
+app.get('/articles/:id', (req, res) => {	
 	Article.findById(id, (err, article) => {
+		if (err) return console.log(err);
 		res.render('edit-article', {article});
 	});
 });
 
+//remove selected article from the database
 app.get('/articles/:id/delete', (req, res) => {
-	const { id } = req.params;
-
 	Article.remove({_id: id}, (err, result) => {
+		if (err) return console.log(err);
 		res.redirect('/view-articles');
 	});
 });
