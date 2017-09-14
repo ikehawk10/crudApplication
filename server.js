9002e8c9b32661b4f1fdfbf55fce685eb583265b
Isaac <hawkins.isaac@gmail.com>
2017-09-14 16:44:01 -0500

    remove console.log statements

--- server.js
+++ server.js
@@ -1,76 +1,73 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const username = 'ikehawk10';
-const access = 'jesus100'
-const data = { 
-		title, 
-		author, 
-		body
-	};
-const id = req.params.id;
-const Schema = mongoose.Schema;
 const app = express();
 
-app.set('view engine', 'ejs');
-//extract data from the form inputs
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://ikehawk10:jesus100@ds133814.mlab.com:33814/articles');
 
-//create schema for database entries
-const articleSchema = new Schema({
-	title: {type: String, required: true},
-	author: {type: String, required: true},
-	body: {type: String, required: true},
+const Article = mongoose.model('articles', {
+	title: String,
+	author: String,
+	body: String
 });
 
-//create mongoose model for data entries
-const Article = mongoose.model('articles', articleSchema);
+app.set('view engine', 'ejs');
+//extract data from the form element
+app.use(bodyParser.urlencoded({extended: true}));
 
-//connect to mLabs database
-mongoose.connect(`mongodb://${username}:${access}@ds133814.mlab.com:33814/articles`);
 
-//homescreen to render index.html
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-//get ALL the articles from the database and render to the page
 app.get('/view-articles', (req, res) => {
 	Article.find({}, (err, articles) => {
-		if (err) return console.log(err);
 		res.render('view-articles', {articles});
 	});
 });
 
-//render newly updated article
-app.post('/articles/:id', (req, res) => {
-	Article.findOneAndUpdate({_id: id}, data, (err, result) => {
-		if (err) return console.log(err);
-		res.redirect('/view-articles');
-	})
-});
 
-//add article to the mongodb collection
+app.post('/articles/:id', (req, res) => {
+	const { id } = req.params
+	const { title, author, body } = req.body
+
+	Article.findOneAndUpdate({_id: id}, { 
+		title, 
+		author, 
+		body
+	}, (err, result) => {
+		res.redirect('/view-articles');
+	});
+})
+
 app.post('/articles', (req, res) => {
+	//add user input into the mongodb collection
+	const { title, author, body } = req.body
+	const data = { 
+		title, 
+		author, 
+		body
+	}
+
 	Article.create(data, (err, result) => {
 		if (err) return console.log(err);
+		//redirect back to homepage
 		res.redirect('/view-articles');
 	})
 })
 
-//get specific article by id to edit
-app.get('/articles/:id', (req, res) => {	
+app.get('/articles/:id', (req, res) => {
+	const { id } = req.params
+
 	Article.findById(id, (err, article) => {
-		if (err) return console.log(err);
 		res.render('edit-article', {article});
 	});
 });
 
-//remove selected article from the database
 app.get('/articles/:id/delete', (req, res) => {
+	const { id } = req.params;
+
 	Article.remove({_id: id}, (err, result) => {
-		if (err) return console.log(err);
 		res.redirect('/view-articles');
 	});
 });
